cee7bae6b15e99b048369bee87c032a54d48c0dd
RodoFranco <s1s73ms@hotmail.com>
2026-09-21 12:35:07 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Pega aquí la URL /exec de tu implementación de Apps Script.
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzoe7TgUEHQ0xPYk5fo5Fnozu3On_fRZIM01NoTkf-43ymQSnolcnlzpY2j8GDtssAT/exec';
