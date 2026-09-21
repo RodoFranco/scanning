fc6e777c988844539e08a4e1d4e3026e6895a4d7
RodoFranco <s1s73ms@hotmail.com>
2026-09-21 13:27:42 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Pega aquí la URL /exec de tu implementación de Apps Script.
+// URL DE GOOGLE APPS SCRIPT YA CONFIGURADA
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzoe7TgUEHQ0xPYk5fo5Fnozu3On_fRZIM01NoTkf-43ymQSnolcnlzpY2j8GDtssAT/exec';
